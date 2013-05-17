4f4dbf4dcffda06d22d99f062997651101a99d04
Bryan Chen <brchen@linkedin.com>
2013-05-17 09:50:57 -0700

    static/css/bootstrap/

--- app.py
+++ app.py
@@ -2,12 +2,16 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return render_template('home.html', name='bryan');
+def home():
+  return render_template('home.html');
 
-@app.route('/about')
-def goodbye():
-    return 'goodbye'
+@app.route('/login')
+def login():
+  return render_template();
+
+@app.route('/map')
+def map():
+  return render_template('map.html');
 
 if __name__ == '__main__':
     app.debug = True
